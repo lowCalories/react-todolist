690a33a984553815c57c500eb7af4ba176e29de2
lowCalories <78588562+lowCalories@users.noreply.github.com>
2021-03-10 23:54:20 +0700

    Rampung migrating Rampung migrating raw html css to react, dan membagi setiap section kedalam komponen masing-masing

--- src/App.js
+++ src/App.js
@@ -2,28 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "./components/Paper";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn">ADD</button>
-        <h1 className="header-title">TODO LIST</h1>
-        <button className="header-btn main-btn-color">CLEAR</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Todo Text</span>
-        </div>
-      </section>
+      <Header />
+      <TodoForm />
+      <Todos />
     </Paper>
   );
 }
